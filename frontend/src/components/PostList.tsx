8435f48aaa8d527a920b5f2af6a169cd39eb5e2a
ASTM1999 <anusornsriprom1999@gmail.com>
2023-10-13 23:44:29 +0700

    แก้การแสดงผลและ เปลี่ยนหลายๆอย่าง

--- frontend/src/components/PostList.tsx
+++ frontend/src/components/PostList.tsx
@@ -6,14 +6,25 @@ import { UserService } from '../services/userServices';
 import { activityServices } from '../services/activityService';
 import { campServices } from '../services/campService';
 import { announcementServices } from '../services/announementService';
+import SearchBar from './SearchBar';
+import { Activity, Users } from '../services/interface';
+import WritingPopup from './WritingPopup';
 
 const PostList = ({ type }: { type: string }) => {
     const posts = useRecoilValue(type === 'activity' ? activityState : type === 'camp' ? campsState : announcementsState);
     const [isEditingMap, setIsEditingMap] = useState<{ [key: string]: boolean }>({});
+    const [isEditing, setIsEditing] = useState<boolean>(false);
+    const [, setSearchText] = useState('');
 
     const [userId, setUserId] = useState<string>()
     const [title, setTitle] = useState<string>()
     const [detail, setDetail] = useState<string>()
+    const [email, setEmail] = useState<string | null>()
+    const [tel, setTel] = useState<string | null>()
+    const [username, setUsername] = useState<string | null>()
+    const [filteredData, setFilteredData] = useState<Activity[]>([]);
+
+    const [userpost, setUserpost] = useState<Users>()
 
     const setActivities = useSetRecoilState(activityState);
     const setCamp = useSetRecoilState(campsState)
@@ -37,10 +48,17 @@ const PostList = ({ type }: { type: string }) => {
             [postId]: true,
         }));
     };
-    const handleUpdateActivity = async (postId: string) => {
+    const handleCancelClick = (postId: string) => {
+        setIsEditingMap((prevState) => ({
+            ...prevState,
+            [postId]: false,
+        }));
+    };
+    const handleUpdateActivity = async (postId: any) => {
         try {
             console.log(postId)
-            await activityServices.updateActivity(postId, title, detail, type);
+            const updateActivity = await activityServices.updateActivity(postId, title, detail, type);
+            console.log("ipdateActivity: ", updateActivity)
             setIsEditingMap((prevState) => ({
                 ...prevState,
                 [postId]: false,
@@ -51,70 +69,129 @@ const PostList = ({ type }: { type: string }) => {
             console.error(`Error updating activity: ${error}`);
         }
     };
-    const handleCancelClick = (postId: string) => {
-        setIsEditingMap((prevState) => ({
-            ...prevState,
-            [postId]: false,
-        }));
-    };
-    async function fetchUser() {
-        const id = await UserService.getUserId()
-        setUserId(id)
+    function formatDate(e: any) {
+        const dateTime = new Date();
+        const justLocalDate = dateTime.toLocaleDateString();
+        return justLocalDate
+    }
+    const fetchUserData = async () => {
+        try {
+            const tel = await UserService.getTel()
+            setTel(tel)
+            const userEmail = await UserService.getEmail()
+            setEmail(userEmail)
+            const id = await UserService.getUserId()
+            setUserId(id)
+            const name = await UserService.getUsername()
+            setUsername(name)
+
+        } catch (error) {
+            console.error("Error fetching user data:", error);
+        }
+    }
+    const handleSearch = (text: any) => {
+        setSearchText(text);
+    }
+    const handleClickPost = async (e: any) => {
+        const filtered = posts.filter((item: any) => item._id === e);
+        const ownerPost = filtered.map((e: any) => e.userOwner)
+        const userPost = await UserService.getUserData(ownerPost[0])
+        setUserpost(userPost)
+        setFilteredData(filtered);
     }
     useEffect(() => {
-        fetchUser()
-    }, [])
+        fetchUserData()
+        console.log("userpost useEffect: ", userpost)
+    }, [userpost])
 
+    const [selectedDateStart, setSelectedDateStart] = useState<Date | null>(new Date());
+    const [selectedDateEnd, setSelectedDateEnd] = useState<Date | null>(new Date());
 
     return (
-        <div className="post" style={{ width: "100%" }}>
-            <ul style={{
-                display: "flex",
-                alignItems: "center",
-                flexDirection: "column"
-            }}>
-                {posts.map((post) => (
-                    <li key={post._id} style={{
-                        width: "80%",
-                        padding: "10px",
-                        backgroundColor: "#D9D9D9",
-                        borderRadius: "8px",
-                    }}>
-                        {isEditingMap[post._id] ? (
-                            <>
-                                <input
-                                    value={title}
-                                    placeholder={post.title}
-                                    onChange={(e) => setTitle(e.target.value)}
-                                /><br />
-                                <input
-                                    value={detail}
-                                    placeholder={post.detail}
-                                    onChange={(e) => setDetail(e.target.value)}
-                                /><br />
-                            </>
-                        ) : (
-                            <>
-                                <h3>{post.title}</h3>
-                                <p>{post.detail}</p>
-                                <p>{post.startDate}</p>
-                                <p>{post.endDate}</p>
-                            </>
+        <div className="post-list">
 
-                        )}
-                        {(post.userOwner === userId) && (
-                            isEditingMap[post._id] ? (
-                                <>
-                                    <button onClick={() => handleUpdateActivity(post._id)}>ยืนยัน</button>
-                                    <button onClick={() => handleCancelClick(post._id)}>ยกเลิก</button>
-                                </>
-                            ) : (
-                                <button onClick={() => handleEditClick(post._id)}>แก้ไข</button>
-                            )
-                        )}
-                    </li>
+            <div className="post">
+                <ul className='postlist-ul'>
+                    <SearchBar onSearch={handleSearch} />
+                    {posts.map((post) => (
+                        <li key={post._id} className='postlist-li'>
+                            <div className='div-post-li' onClick={() => handleClickPost(post._id)}>
+                                <h2>{post.title}</h2>
+                                <p>{formatDate(post.startDate)} - {formatDate(post.endDate)}</p>
+                            </div>
+
+                            {/* )} */}
+                            {(post.userOwner === userId) && (
+                                isEditingMap[post._id] ? (
+                                    <>
+                                        <button onClick={() => handleUpdateActivity(post._id)}>ยืนยัน</button>
+                                        <button onClick={() => handleCancelClick(post._id)}>ยกเลิก</button>
+                                    </>
+                                ) : (
+                                    <button onClick={() => handleEditClick(post._id)}>แก้ไข</button>
+                                )
+                            )}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+
+            <div className="contantActivity">
+                {filteredData.map((item) => (
+                    <>
+                        <div className="head-contantActivity">
+                            <h1>Contact Poster</h1>
+                            <div className="cpname">
+                                <p>Post by</p>
+                                <p>{userpost?.username}</p>
+                                <p>{userpost?.email}</p>
+                            </div>
+
+                            <div className="cptel">
+                                <p>เบอร์ติดต่อ: </p>
+                                <p>{userpost?.tel}</p>
+                            </div>
+                        </div>
+
+                        <div className="body-contantActivity" key={item._id}>
+                            <div className="hbody-contantActivity">
+                                <h2>
+                                    {item.title}
+                                </h2>
+                            </div>
+                            <div className="dbody-containActivity">
+                                {item.detail}
+                            </div>
+                        </div>
+                        <div className="bt-edit">
+                            {(item.userOwner === userId) && (
+                                !isEditingMap[item._id] ? (
+                                    <div className="" style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+                                        <button className='bt-st-sc' onClick={() => handleEditClick(item._id)}>แก้ไข</button>
+                                    </div>
+                                ) : (
+                                    <>
+                                        <WritingPopup
+                                            title={item.title}
+                                            detail={item.detail}
+                                            selectedDateStart={selectedDateStart}
+                                            selectedDateEnd={selectedDateEnd}
+                                            onStartDate={(date) => setSelectedDateStart(date)}
+                                            onEndDate={(date) => setSelectedDateEnd(date)}
+                                            onTitleChange={(e) => setTitle(e.target.value)}
+                                            onDetailChange={(e) => setDetail(e.target.value)}
+                                            onConfirm={() => handleUpdateActivity(item._id)}
+                                            onCancel={() => handleCancelClick(item._id)}
+                                        />
+                                    </>
+                                )
+
+                            )}
+                        </div>
+                    </>
                 ))}
-            </ul>
+            </div>
+
         </div>
     );
 };
